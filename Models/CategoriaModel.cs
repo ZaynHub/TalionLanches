16bb4a3af61c6bc093d9a60f7f6eb7921295379d
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-23 15:01:15 -0300

    Added data annotations

--- Models/CategoriaModel.cs
+++ Models/CategoriaModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TalionLanches.Models {
   public class CategoriaModel {
     public int Id { get; set; }
+
+    [StringLength(100)]
     public string Nome { get; set; }
+
+    [StringLength(200)]
     public string Descricao { get; set; }
+
     public List<LancheModel> Lanches { get; set; }
+
   }
 }
--- Models/LancheModel.cs
+++ Models/LancheModel.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TalionLanches.Models {
   public class LancheModel {
     public int Id { get; set; }
+
+    [StringLength(100)]
     public string Nome { get; set; }
+
+    [StringLength(100)]
     public string DescricaoCurta { get; set; }
+
+    [StringLength(255)]
     public string DescricaoDetalhada { get; set; }
-    public Decimal Preco { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Preco { get; set; }
+
+    [StringLength(200)]
     public string ImagemUrl { get; set; }
+
+    [StringLength(200)]
     public string ImagemThumbnailUrl { get; set; }
+
     public bool IsLanchePreferido { get; set; }
+
     public bool EmEstoque { get; set; }
+
     public int CategoriaId { get; set; }
+
     public virtual CategoriaModel Categoria { get; set; }
+
   }
 }
