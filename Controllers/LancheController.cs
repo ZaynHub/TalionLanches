a51131dfcbd37d05b14f1eea25190aabe4fdeead
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-23 18:04:10 -0300

    Added Lanche Controller

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalionLanches.Repositories.Interfaces;
+
+namespace TalionLanches.Controllers {
+  public class LancheController : Controller {
+    private readonly ILancheRepository _lancheRepository;
+    private readonly ICategoriaRepository _categoriaRepository;
+
+    public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository) {
+      _lancheRepository = lancheRepository;
+      _categoriaRepository = categoriaRepository;
+    }
+
+    public IActionResult List() {
+      ViewBag.Lanche = "Lanche";
+      ViewData["Categoria"] = "Categoria";
+
+      var lanches = _lancheRepository.Lanches;
+      return View(lanches);
+    }
+  }
+}
