cf604ddc8bf9e6fe98fdec6e1cc1f5b3944f5156
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-23 14:38:50 -0300

    Added db context

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalionLanches.Models;
+
+namespace TalionLanches.Context {
+  public class AppDbContext : DbContext {
+
+    public DbSet<LancheModel> Lanches { get; set; }
+    public DbSet<CategoriaModel> Categorias { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+
+    }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TalionLanches.Context;
 
 namespace TalionLanches {
   public class Startup {
@@ -19,6 +21,7 @@ namespace TalionLanches {
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
+      services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
       services.AddControllersWithViews();
     }
 
