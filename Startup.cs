8b6c16adc1cd8d3067423503950427e3268046b4
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-09-23 17:00:39 -0300

    enabled Razor Runtime Compilation

--- Startup.cs
+++ Startup.cs
@@ -15,19 +15,25 @@ using TalionLanches.Repositories.Interfaces;
 
 namespace TalionLanches {
   public class Startup {
-    public Startup(IConfiguration configuration) {
-      Configuration = configuration;
-    }
-
     public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Env { get; }
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env) {
+      Configuration = configuration;
+      Env = env;
+    }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
+
       services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
       services.AddTransient<ICategoriaRepository, CategoriaRepository>();
       services.AddTransient<ILancheRepository, LancheRepository>();
 
-      services.AddControllersWithViews();
+      var builder = services.AddControllersWithViews();
+      if (Env.IsDevelopment()) {
+        builder.AddRazorRuntimeCompilation();
+      }
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
